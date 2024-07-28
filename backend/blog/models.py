3e9f4ab552043fdc5367326c2ba8461ad71883da
Hassan EL BAHI <me_80_me@hotmai.com>
2024-07-29 00:13:30 +0100

    feat: Add slug field to Category model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
-
 # Create your models here.
 class Category(models.Model):
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+ 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
