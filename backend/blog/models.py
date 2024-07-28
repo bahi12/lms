1060f65d60ebe2540c28a683d8579ed9266aebc8
Hassan EL BAHI <me_80_me@hotmai.com>
2024-07-29 00:08:06 +0100

    feat: Add slug field to Post model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    post = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=255, unique=True)  # Add this line
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.title
