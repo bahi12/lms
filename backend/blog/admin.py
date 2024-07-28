e85d9a23c1403aa5bca38455c36cce134e6e219b
Hassan EL BAHI <me_80_me@hotmai.com>
2024-07-29 00:08:28 +0100

    feat: Add Category model and admin functionality

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Category
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created')
+    list_filter = ('category', 'created')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('-created',)
+    date_hierarchy = 'created'
+
+admin.site.register(Post, PostAdmin)
