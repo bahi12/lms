553d07627f1893da988a026ca6e158f1e36130ac
Hassan EL BAHI <me_80_me@hotmai.com>
2024-07-29 15:41:30 +0100

    chore: Add frontend files and configurations for initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+    <div>
+      <header>Blog App</header>
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
