40a6b239c455161dcc4c187ecf1eedda6070ce8c
Hassan EL BAHI <me_80_me@hotmai.com>
2024-07-29 15:42:39 +0100

    feat: Add Footer component for displaying the website footer

---
+++ frontend/src/template/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="py-5 bg-dark">
+      <div className="container px-4 px-lg-5"><p className="m-0 text-center text-white">Copyright &copy; HelB Portal 2023</p></div>
+    </footer>
+  );
+}
+
+export default Footer;
