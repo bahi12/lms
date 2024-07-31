d945931a75e2376219df49741efbc15754ad0bad
Hassan EL BAHI <me_80_me@hotmai.com>
2024-07-31 19:53:04 +0100

    refactor: Remove login_required decorator from URL patterns

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
 from .views import (
     PostListView, PostRetrieveView, PostUpdateView, PostDestroyView,
     CategoryListCreateView, CategoryRetrieveView, CategoryUpdateView, CategoryDestroyView,
@@ -10,17 +9,19 @@ urlpatterns = [
     ### Post URLs
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostRetrieveView.as_view(), name='post-detail'),
-    path('post/create/', login_required(PostCreateView.as_view()), name='post-create'),
-    path('post/<int:pk>/update/', login_required(PostUpdateView.as_view()), name='post-update'),
-    path('post/<int:pk>/delete/', login_required(PostDestroyView.as_view()), name='post-delete'),
+    path('post/create/', (PostCreateView.as_view()), name='post-create'),
+    path('post/<int:pk>/update/', (PostUpdateView.as_view()), name='post-update'),
+    path('post/<int:pk>/delete/', (PostDestroyView.as_view()), name='post-delete'),
     
     ### Category URLs
     path('category/', CategoryListCreateView.as_view(), name='category-list'),
     path('category/<int:pk>/', CategoryRetrieveView.as_view(), name='category-detail'),
-    path('category/create/', login_required(CategoryListCreateView.as_view()), name='category-create'),
-    path('category/<int:pk>/update/', login_required(CategoryUpdateView.as_view()), name='category-update'),
-    path('category/<int:pk>/delete/', login_required(CategoryDestroyView.as_view()), name='category-delete'),
+    path('category/create/', (CategoryListCreateView.as_view()), name='category-create'),
+    path('category/<int:pk>/update/', (CategoryUpdateView.as_view()), name='category-update'),
+    path('category/<int:pk>/delete/', (CategoryDestroyView.as_view()), name='category-delete'),
     
     ### File Upload URL
     path('upload/', custom_upload_file, name='custom_upload_file'),
+
+
 ]
