03473f24da9c07df71f1eca38577e97cc6b0b09c
Hassan EL BAHI <me_80_me@hotmai.com>
2024-07-31 19:54:27 +0100

    feat: Update user views to use Django default authentication views

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# use django default aujthentication views
+from django.contrib.auth.views import LoginView, LogoutView
+class Login(LoginView):
+    template_name = 'login.html'
 
-# Create your views here.
+class Logout(LogoutView):
+    template_name = 'logout.html'
