0416f10f1ab67016867bb2938b969199edd2d8fa
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-01 23:36:02 +0100

    refactor: Add UserSerializer and LoginSerializer to user/serializers.py

---
+++ backend/user/serializers.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from backend.core.settings import AUTH_USER_MODEL
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from backend.user.apps import UserConfig
+
+
+User = settings.AUTH_USER_MODEL
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+        user = authenticate(username=username, password=password)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials!")
