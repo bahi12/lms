4f28e98e23d4ed9ce3da03ff12b64c2df9cb2e52
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-01 23:36:18 +0100

    refactor: Update user views to use Django default authentication views

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 # use django default aujthentication views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+import rest_framework
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer, LoginSerializer
 class Login(LoginView):
-    template_name = 'login.html'
+    permission_class = (AllowAny,)
+    serialize_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer..validated_data
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access' : str(refresh.access_token,)
+        })
 
 class Logout(LogoutView):
     template_name = 'logout.html'
+
+class Register(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_class = (AllowAny,)
+    serializer_class = UserSerializer
+
+
+
