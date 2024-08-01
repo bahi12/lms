bca9394e43ea208d1a58866d71f9ae537e48703e
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-01 23:35:55 +0100

    refactor: Update user account URLs to use 'auth' instead of 'accounts'

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
-    path('accounts/', include('user.urls')),
+    path('auth/', include('user.urls')),
     
 ]
 
