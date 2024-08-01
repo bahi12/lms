672ba7dc14a2bc37ee757b82b804f11d1c63ae6d
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-01 23:56:41 +0100

    refactor: Update Login view to use generics.GenericAPIView

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render
-# use django default aujthentication views
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.models import User
-import rest_framework
+from django.contrib.auth import get_user_model
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer, LoginSerializer
-class Login(LoginView):
-    permission_class = (AllowAny,)
-    serialize_class = LoginSerializer
+
+User = get_user_model()
+
+class Login(generics.GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer..validated_data
+        user = serializer.validated_data
         refresh = RefreshToken.for_user(user)
         return Response({
             'refresh': str(refresh),
-            'access' : str(refresh.access_token,)
+            'access': str(refresh.access_token),
         })
 
 class Logout(LogoutView):
@@ -26,8 +27,5 @@ class Logout(LogoutView):
 
 class Register(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_class = (AllowAny,)
+    permission_classes = (AllowAny,)
     serializer_class = UserSerializer
-
-
-
