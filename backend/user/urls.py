84aebe4705c976631d5d4d9747e0678754762b11
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-01 23:36:09 +0100

    feat: Add registration endpoint and token refresh endpoint

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Login, Logout
+from .views import Register, Login
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    path('register/', Register.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
-    path('logout/', Logout.as_view(), name='logout'),
-]
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    ]
