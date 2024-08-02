d3255e1d0f3cb208fc24d8cf458e2ab961978f9c
Hassan EL BAHI <me_80_me@hotmail.com>
2024-08-02 16:22:44 +0100

    refactor: Improve Card component by limiting displayed content length

--- frontend/src/template/Card.js
+++ frontend/src/template/Card.js
@@ -4,13 +4,19 @@ import Col from 'react-bootstrap/Col';
 
 function Card({ title, post, linkText = 'Read More' }) {
   const sanitizedHtml = DOMPurify.sanitize(post);
+  const words = sanitizedHtml.split(' ').slice(0, 50).join(' ');
+
+  let content = words;
+  if (sanitizedHtml.split(' ').length > 50) {
+    content += '(...)';
+  }
 
   return (
     <Col md={4} className="mb-5">
       <div className="card h-100">
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
-          <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
+          <div dangerouslySetInnerHTML={{ __html: content }} />
         </div>
         <div className="card-footer">
           <a className="btn btn-primary btn-sm" href="#!">{linkText}</a>
