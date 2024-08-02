5c51e9f6d646509b6ebb6db4ec47fff80d5cd40e
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-02 01:20:20 +0100

    feat: Add Home component with PostList and Layout components

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PostList from './PostList';
+import Layout from '../template/Layout';
+
+
+function Home() {
+  return (
+    <div>
+        
+            <PostList />
+        
+    </div>
+  );
+}
+
+export default Home;
