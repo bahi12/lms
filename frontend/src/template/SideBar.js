9c099a0d5cd87cb929a0a91b42f30a559f89842a
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-02 01:20:51 +0100

    refactor: Add SideBar component for navigation links

---
+++ frontend/src/template/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function SideBar() {
+    return (
+        <div className="sidebar">
+        <Link to="#">Bac 2</Link>
+        <Link to="#">Bac 1</Link>
+        <Link to="#">Common Core</Link>
+        </div>
+    );
+    };
+      
+export default SideBar;
+      
