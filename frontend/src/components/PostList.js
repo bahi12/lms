ae52a5fe53a49649725976cdab082a308f545661
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-02 01:21:12 +0100

    refactor: Update PostList component to use react-bootstrap Container and Row

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import Card from '../template/Card'; // Import the Card component
 
-function   
- PostList() {
+function PostList() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/api/');   
-
+        const response = await axios.get('http://localhost:8000/api/');
         setPosts(response.data);
       } catch (error) {
         console.error('Error fetching posts:', error);
@@ -21,11 +21,13 @@ function  
   }, []);
 
   return (
-    <div>
-      {posts.map(post => (
-        <Card key={post.id} title={post.title} post={post.post} linkText="Read More" />
-      ))}
-    </div>
+    <Container>
+      <Row>
+        {posts.map(post => (
+          <Card key={post.id} title={post.title} post={post.post} linkText="Read More" />
+        ))}
+      </Row>
+    </Container>
   );
 }
 
