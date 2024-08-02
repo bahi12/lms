0f80ff02724b719c17f6a5d90d849f8578038c1e
Hassan EL BAHI <me_80_me@hotmail.com>
2024-08-02 16:19:38 +0100

    feat: Add login page and 404 error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './template/Layout';
 import Home from './components/Home';
+import Login from './components/auth/Login';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="login/" element={<Login />} />
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Routes>
     </Layout>
   );
+}
+
+function NotFound() {
+  return <h1>404 - Page Not Found</h1>;
 }
 
 export default App;
