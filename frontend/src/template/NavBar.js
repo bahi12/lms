b6bfe4efbee5ce7d8c460beeb3a42f15c3ee2fdf
Hassan EL BAHI <me_80_me@hotmail.com>
2024-08-02 16:39:19 +0100

    chore: Update NavBar component to include login/logout functionality

--- frontend/src/template/NavBar.js
+++ frontend/src/template/NavBar.js
@@ -1,31 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 function NavBar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <Link className="navbar-brand" to="/">My Website</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/about">About</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/contact">Contact</Link>
-                    </li>
-                </ul>
+    const isLoggedIn = false; // Replace with your login logic
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <div className="d-flex ms-auto">
+                    <Link className="navbar-brand" to="/">
+                        My Website
+                    </Link>
+                </div>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+            {isLoggedIn ? (
+                <Link className="btn btn-primary me-2" to="/logout/">Logout</Link>
+            ) : (
+                <Link className="btn btn-primary me-2" to="/login/">Login</Link>
+            )}
+                </div>
             </div>
-        </div>
-    </nav>
-);
-};
+        </nav>
+    );
+}
 
 export default NavBar;
