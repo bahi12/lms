c3ee66ce9f03c5615a7b66d5a53cc65ebe3b293f
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-02 01:20:59 +0100

    refactor: Update NavBar component to use react-router-dom for navigation links

--- frontend/src/template/NavBar.js
+++ frontend/src/template/NavBar.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+
+function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container px-5">
-        <a className="navbar-brand" href="#!">Start Bootstrap</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a></li>
-            <li className="nav-item"><a className="nav-link" href="#!">About</a></li>
-            <liclassName="nav- className="nav-item"><a className="nav-link" href="#!">Services</a></li>
-          </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <Link className="navbar-brand" to="/">My Website</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/about">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
     </nav>
-  );
-}
+);
+};
 
-export default Navbar;
+export default NavBar;
