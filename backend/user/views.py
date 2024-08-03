6976574a72e7550e14ceec1b28464028103a2126
Hassan EL BAHI <me_80_me@hotmail.com>
2024-08-03 23:31:18 +0100

    refactor: Update user views and serializers

--- backend/user/views.py
+++ backend/user/views.py
@@ -10,31 +10,6 @@ from rest_framework.views import APIView
 
 User = get_user_model()
 
-class Login(generics.GenericAPIView):
-    permission_classes = (AllowAny,)
-    serializer_class = LoginSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
-        refresh = RefreshToken.for_user(user)
-        return Response({
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        })
-
-class Logout(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, *args, **kwargs):
-        try:
-            refresh_token = request.data.get('refresh')
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response({"message": "Logged out!"}, status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 class UserDetail(APIView):
     permission_classes = (IsAuthenticated,)
 
@@ -47,8 +22,3 @@ class UserDetail(APIView):
             # Add more fields if needed
         }
         return Response(user_data)
-
-class Register(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (AllowAny,)
-    serializer_class = UserSerializer
