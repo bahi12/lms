fcab85de0d558a03925ea5282313e2a6dd6dae64
Hassan EL BAHI <me_80_me@hotmail.com>
2024-08-03 23:31:12 +0100

    refactor: Update user URLs and remove unused views and imports

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import Register, Login, UserDetail, Logout
-from rest_framework_simplejwt.views import TokenRefreshView
+from .views import UserDetail
+
 
 urlpatterns = [
-    path('register/', Register.as_view(), name='register'),
-    path('login/', Login.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('user/', UserDetail.as_view(), name='user'),
-    path('logout/', Logout.as_view(), name='logout'),
+ 
     ]
