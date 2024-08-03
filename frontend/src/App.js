c4c20790b826e47eface128ec2663d36c833dd4e
Hassan EL BAHI <me_80_me@hotmail.com>
2024-08-03 15:01:56 +0100

    refactor: Update 404 page route in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login/" element={<Login />} />
-        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        <Route path="*" element={NotFound} />
       </Routes>
     </Layout>
   );
