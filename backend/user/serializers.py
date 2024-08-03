cf18e257c376fe701f4419681702687e9752a3f8
Hassan EL BAHI <me_80_me@hotmail.com>
2024-08-03 23:31:05 +0100

    refactor: Remove unused LoginSerializer from user serializers

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -22,14 +22,3 @@ class UserSerializer(serializers.ModelSerializer):
         )
         return user
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    def validate(self, data):
-        username = data.get('username')
-        password = data.get('password')
-        user = authenticate(username=username, password=password)
-        if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Invalid credentials!")
