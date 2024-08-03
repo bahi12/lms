4c12755f835da93b9169fbf78cbc286fae7bebf9
Hassan EL BAHI <me_80_me@hotmail.com>
2024-08-03 16:20:06 +0100

    refactor: Remove unused import in Home component

--- frontend/src/redux/reducer.js
+++ frontend/src/redux/reducer.js
@@ -3,7 +3,6 @@ import {
     LOGIN_FAIL,
     USER_LOADED_SUCCESS,
     USER_LOADED_FAIL,
-    LOGOUT
 } from './actions';
 
 const initialState = {
@@ -23,13 +22,11 @@ const authReducer = (state = initialState, action) => {
                 isLoggedIn: true,
             };
         case LOGIN_FAIL:
-        case LOGOUT:
             return {
                 ...state,
                 access: null,
                 refresh: null,
                 isLoggedIn: false,
-                user: null,
             };
         case USER_LOADED_SUCCESS:
             return {
