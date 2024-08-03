50372561829614f6ed44cc001605c224605296c0
Hassan EL BAHI <me_80_me@hotmail.com>
2024-08-03 16:19:59 +0100

    refactor: Update Redux actions and reducer for authentication

--- frontend/src/redux/actions.js
+++ frontend/src/redux/actions.js
@@ -4,7 +4,6 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const USER_LOADED_SUCCESS = 'USER_LOADED_SUCCESS';
 export const USER_LOADED_FAIL = 'USER_LOADED_FAIL';
-export const LOGOUT = 'LOGOUT';
 
 export const loginSuccess = (access, refresh) => ({
     type: LOGIN_SUCCESS,
@@ -24,39 +23,20 @@ export const userLoadedFail = () => ({
     type: USER_LOADED_FAIL,
 });
 
-export const logout = () => {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    return { type: LOGOUT };
-};
-
 export const loginUser = (username, password) => async (dispatch) => {
     try {
         const response = await axios.post('http://localhost:8000/api/auth/login/', { username, password });
         const { access, refresh } = response.data;
-        localStorage.setItem('accessToken', access);
-        localStorage.setItem('refreshToken', refresh);
         dispatch(loginSuccess(access, refresh));
+
+        // Load the user data after login
+        const userResponse = await axios.get('http://localhost:8000/api/auth/user/', {
+            headers: {
+                Authorization: `Bearer ${access}`
+            }
+        });
+        dispatch(userLoadedSuccess(userResponse.data));
     } catch (error) {
         dispatch(loginFail());
-        throw error;
-    }
-};
-
-export const loadUser = () => async (dispatch) => {
-    try {
-        const accessToken = localStorage.getItem('accessToken');
-        if (accessToken) {
-            const response = await axios.get('http://localhost:8000/api/auth/user/', {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            });
-            dispatch(userLoadedSuccess(response.data));
-        } else {
-            dispatch(userLoadedFail());
-        }
-    } catch (error) {
-        dispatch(userLoadedFail());
     }
 };
