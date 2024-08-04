91794bd73993de191d21c1010ce6ff424c3a0fd9
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-04 18:48:44 +0100

    refactor: Add PostItem component for displaying posts in different views

---
+++ frontend/src/components/PostItem.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const PostItem = ({ post, view }) => {
+  const renderPost = (text) => {
+    if (text.split(' ').length > 50) {
+      return `${text.split(' ').slice(0, 50).join(' ')}...`;
+    }
+    return text;
+  };
+
+  if (view === 'cards') {
+    return (
+      <Col md={4} className="mb-5">
+        <Card className="h-100">
+          <Card.Body>
+            <Card.Title>{post.title}</Card.Title>
+            <Card.Text>
+              <p dangerouslySetInnerHTML={{ __html: renderPost(post.post) }}></p>
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Button variant="primary" size="sm" href="#!">Read More</Button>
+          </Card.Footer>
+        </Card>
+      </Col>
+    );
+  } else if (view === 'list') {
+    return (
+      <ListGroup.Item>
+        <h5>{post.title}</h5>
+        <p dangerouslySetInnerHTML={{ __html: renderPost(post.post) }}></p>
+      </ListGroup.Item>
+    );
+  }
+};
+
+export default PostItem;
