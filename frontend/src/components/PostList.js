d0003654961b662d972203ceadacbd171aa18295
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-04 18:49:27 +0100

    refactor: Add functionality for two views of posts

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Card from '../template/Card'; // Import the Card component
+import ListGroup from 'react-bootstrap/ListGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import PostItem from './PostItem'; // Import the new PostItem component
 
 function PostList() {
   const [posts, setPosts] = useState([]);
+  const [view, setView] = useState('cards');
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/api/');
-        setPosts(response.data);
+        const response = await fetch('http://localhost:8000/api/');
+        if (!response.ok) {
+          throw new Error('Error fetching posts');
+        }
+        const data = await response.json();
+        setPosts(data);
       } catch (error) {
         console.error('Error fetching posts:', error);
       }
@@ -20,13 +27,45 @@ function PostList() {
     fetchPosts();
   }, []);
 
+  const handleViewChange = (selectedView) => {
+    setView(selectedView);
+    localStorage.setItem('selectedView', selectedView);
+  };
+
+  useEffect(() => {
+    const savedView = localStorage.getItem('selectedView');
+    if (savedView) {
+      setView(savedView);
+    }
+  }, []);
+
+  const renderPosts = () => {
+    if (view === 'cards') {
+      return (
+        <Row>
+          {posts.map(post => (
+                <PostItem key={post.id} post={post} view={view} />
+          ))}
+        </Row>
+      );
+    } else if (view === 'list') {
+      return (
+        <ListGroup>
+          {posts.map(post => (
+            <PostItem key={post.id} post={post} view={view} />
+          ))}
+        </ListGroup>
+      );
+    }
+  };
+
   return (
     <Container>
-      <Row>
-        {posts.map(post => (
-          <Card key={post.id} title={post.title} post={post.post} linkText="Read More" />
-        ))}
-      </Row>
+      <DropdownButton id="dropdown-basic-button" title="Select View" onSelect={handleViewChange}>
+        <Dropdown.Item eventKey="cards">Cards</Dropdown.Item>
+        <Dropdown.Item eventKey="list">List</Dropdown.Item>
+      </DropdownButton>
+      {renderPosts()}
     </Container>
   );
 }
