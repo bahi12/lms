d7efbe0dd83af8c33e86bb878397a2f26823a4ae
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-04 07:33:48 +0100

    refactor: Update ReactDOM import to use createRoot for improved rendering performance

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import createRoot from react-dom/client
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root element
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
