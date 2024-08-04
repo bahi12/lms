05b42aaf959facc3c0f4ffa64499306c7e8ba350
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-04 18:50:06 +0100

    refactor: Update SideBar component to use react-bootstrap and NavLink

--- frontend/src/template/SideBar.js
+++ frontend/src/template/SideBar.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Container, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
 
 function SideBar() {
     return (
         <div className="sidebar">
-            <ul className="list-group">
-                <li className="list-group-item"><Link to="#">Bac 2</Link></li>
-                <li className="list-group-item"><Link to="#">Bac 1</Link></li>
-                <li className="list-group-item"><Link to="#">Common Core</Link></li>
-            </ul>
-        </div>
+        <Container>
+          <Nav className="flex-column">
+            <Nav.Link as={NavLink} to="/" activeClassName="active">
+              Dashboard
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/tables" activeClassName="active">
+              Tables
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/profile" activeClassName="active">
+              Profile
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/analytics" activeClassName="active">
+              Analytics
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/hero404" target="_blank" activeClassName="active">
+              404 Page
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </div>
     );
     };
       
