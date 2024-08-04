0cfc36e501d304c4432b935db232e781efde0a34
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-04 06:44:24 +0100

    feat: Add registration functionality and error handling in Register component

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,35 +1,43 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [re_password, setRePassword] = useState('');
     const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 
     const handleRegister = async () => {
         try {
-            const response = await fetch('/api/auth/register', {
+            const response = await fetch('http://localhost:8000/api/auth/users/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ username, password, email }),
+                body: JSON.stringify({ username, password, re_password, email }),
             });
 
             if (response.ok) {
-                // Registration successful, handle the response accordingly
-                console.log('Registration successful');
+                setMessage('Registration successful');
+                setTimeout(() => {
+                    navigate('/me/');
+                }, 3000); // 3 seconds delay
             } else {
-                // Registration failed, handle the error response accordingly
-                console.error('Registration failed');
+                const data = await response.json();
+                setMessage(data[Object.keys(data)[0]][0]); // Show the first error message
             }
         } catch (error) {
             console.error('An error occurred during registration', error);
+            setMessage('An error occurred during registration');
         }
     };
 
     return (
         <div>
             <h2>Register</h2>
+            {message && <p>{message}</p>}
             <form>
                 <label>
                     Username:
@@ -46,6 +54,15 @@ const Register = () => {
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Confirm Password:
+                    <input
+                        type="password"
+                        value={re_password}
+                        onChange={(e) => setRePassword(e.target.value)}
                     />
                 </label>
                 <br />
