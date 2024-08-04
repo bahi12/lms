d43ed87ef70d1c1667b9d17fed9e4faf4a0a855b
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-04 07:33:39 +0100

    refactor: Add protected route for user profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './template/Layout';
 import Home from './components/Home';
 import Login from './components/auth/Login';
 import Logout from './components/auth/Logout';
 import Register from './components/auth/Register';
+import Profile from './components/Profile';
+import { useSelector } from 'react-redux';
+
+
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login/" element={<Login />} />
+        <Route 
+                    path="/me" 
+                    element={isLoggedIn ? <Profile /> : <Navigate to="/login" />} 
+        />
         <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={NotFound} />
