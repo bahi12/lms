e23632920eef52f844c408ce8a55029d985b6a50
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-04 11:16:35 +0100

    refactor: Add login check to Profile component

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,15 +1,22 @@
-// Profile.js
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUser } from '../redux/actions';
+import Login from './auth/Login';
+// Profile.js
 
 const Profile = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
+    const isLoggedIn = useSelector((state) => state.isLoggedIn);
 
     useEffect(() => {
         dispatch(fetchUser());
     }, [dispatch]);
+
+    if (!isLoggedIn) {
+        return <div>Please log in to view your profile. <Login /></div>;
+
+    }
 
     if (!user) {
         return <div>Loading...</div>;
