eac4c36a2529876f24035406fe7d7c8aef06189f
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-05 11:47:02 +0100

    refactor: Update SideBar component to use consistent class name for sidebar links

--- frontend/src/template/SideBar.js
+++ frontend/src/template/SideBar.js
@@ -2,32 +2,30 @@ import React from 'react';
 import { Container, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
-
 function SideBar() {
-    return (
-        <div className="sidebar">
-        <Container>
-          <Nav className="flex-column">
-            <Nav.Link as={NavLink} to="/" activeClassName="active">
-              Dashboard
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/tables" activeClassName="active">
-              Tables
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/profile" activeClassName="active">
-              Profile
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/analytics" activeClassName="active">
-              Analytics
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/hero404" target="_blank" activeClassName="active">
-              404 Page
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </div>
-    );
-    };
-      
+  return (
+    <div className="sidebar">
+      <Container>
+        <Nav className="flex-column">
+          <Nav.Link as={NavLink} to="/" activeClassName="active" className="sidebar-link">
+            Dashboard
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/tables" activeClassName="active" className="sidebar-link">
+            Tables
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/profile" activeClassName="active" className="sidebar-link">
+            Profile
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/analytics" activeClassName="active" className="sidebar-link">
+            Analytics
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/hero404" target="_blank" activeClassName="active" className="sidebar-link">
+            404 Page
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </div>
+  );
+}
+
 export default SideBar;
-      
