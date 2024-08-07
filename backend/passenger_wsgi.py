0db9087952776a7b5ce0378f0e4356e43c6d0695
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-07 11:59:37 +0100

    refactor: Remove virtual environment activation from passenger_wsgi.py

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -8,10 +8,5 @@ sys.path.append('/home/bahi12/dev/lms/backend')
 # Set the DJANGO_SETTINGS_MODULE environment variable
 os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 
-# Activate your virtual environment (if applicable)
-activate_this = '/home/bahicom/virtualenv/repositories/lms/backend/3.9/bin/activate'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-
 # Import the Django application
 application = get_wsgi_application()
