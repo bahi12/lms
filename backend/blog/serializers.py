50c0cddc4f97c29176fd112e15deaa6d8600927a
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-09 00:12:13 +0100

    refactor: Update PostSerializer to include category information
    The PostSerializer in the serializers.py file has been updated to include the category information by using the CategorySerializer. This change ensures that the category details are included when serializing a post.
    
    Closes #789

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from .models import Category, Post
 
-class PostSerializer(serializers.ModelSerializer):
-    def perform_update(self, serializer):
-        serializer.save(post=self.get_object())
-    class Meta:
-        model = Post
-        fields = '__all__'
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+        fields = ['id', 'name']
+
+class PostSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = Post
         fields = '__all__'
