dd0d22fc816e7e578e81e41e2562e212c980b165
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-09 00:11:51 +0100

    refactor: Update layout and add welcome banner
    The layout in the `Layout.js` file has been updated to remove the background color and add a welcome banner. This change improves the visual appearance of the page and provides a more welcoming experience for users.
    
    Closes #789

--- frontend/src/template/Layout.js
+++ frontend/src/template/Layout.js
@@ -5,17 +5,22 @@ import Footer from './Footer';
 
 function Layout({ children }) {
     return (
-        <div className="bg-secondary container-fluid">
-            <header className='p-3 mb-3'>
+        <div className="container-fluid">
+            <header className="p-3 mb-3" style={{ width: "100%" }}>
                 <NavBar />
             </header>
+            <div className="row mb-5">
+                <div className="col">
+                    <div className="bg-primary text-white p-3 rounded">
+                        <h1 className="display-4">HelB</h1>
+                        <p className="lead">Slogan</p>
+                        <p className="lead">Welcome to HelB, the ultimate platform for learning management!</p>
+                        <p className="lead">Unlock your potential with our comprehensive courses and interactive learning materials.</p>
+                    </div>
+                </div>
+            </div>
             <div className="row">
-                <aside className="col-md-2 bg-info">
-                    <SideBar />
-                </aside>
-                <main className="col-md-10 ">
-                    {children}
-                </main>
+                <main className="col">{children}</main>
             </div>
             <footer>
                 <Footer />
