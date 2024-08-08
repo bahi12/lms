bb3004d88e63afeb4a50986a10b924a520f8f858
Hassan EL BAHI <me_80_me@hotmai.com>
2024-08-09 00:12:05 +0100

    refactor: Update Footer component styling
    Update the styling of the Footer component in the `Footer.js` file. Change the background color to dark and the text color to white to improve the visual appearance of the footer.
    
    Closes #789

--- frontend/src/template/Footer.js
+++ frontend/src/template/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
 return (
-    <footer className="bg-light text-center text-lg-start">
+    <footer className="bg-dark text-center text-white">
             <div className="text-center p-3">
                     &copy; 2023 My Website
             </div>
